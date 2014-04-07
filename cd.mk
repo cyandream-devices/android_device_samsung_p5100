c095e76602705f5b5298dab0d061c69f954f197f
yanniks <kontakt@yanniks.de>
2014-04-07 20:38:48 +0200

    initial CyanDream commit
    Change-Id: I3bf435c43876910259b78034981bc306871ad0bf

--- cd.mk
+++ cd.mk
@@ -15,7 +15,7 @@
 #
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := p5100
@@ -25,14 +25,14 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 1280
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/p5100/full_p5100.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p5100
-PRODUCT_NAME := cm_p5100
+PRODUCT_NAME := cd_p5100
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-P5100
 PRODUCT_MANUFACTURER := samsung
